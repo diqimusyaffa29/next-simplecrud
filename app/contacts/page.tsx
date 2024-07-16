066126533c094aa75433f05a0e64a3ccc929f52e
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-07-16 17:17:01 +0800

    Successfull adding add contact function

---
+++ app/contacts/page.tsx
@@ -0,0 +1,16 @@
+import { CreateButton } from "@/components/button";
+import ContactTable from "@/components/contact-table";
+import Search from "@/components/search";
+const Contacts = () => {
+    return (
+        <div className="max-w-screen-md mx-auto mt-5 ">
+            <div className="flex item-center justify-between gap-1 mb-5">
+                <Search />
+                <CreateButton/>
+            </div>
+            <ContactTable />
+        </div>
+    );
+};
+
+export default Contacts;
---
+++ components/create-form.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {saveContact} from "@/lib/action";
+import {useFormState} from "react-dom";
+
+const CreateForm = () => {
+    const [state, formAction] = useFormState(saveContact, null);
+    return (
+        <div>
+            <form action={formAction}>
+                <div className="mb-5">
+                    <label htmlFor="name" className="block text-sm font-medium text-gray-900">
+                        Full Name
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Full Name..."
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    />
+                    <div id="name-error" arial-live="polite" aria-atomic="true">
+                        <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
+                    </div>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="phone" className="block text-sm font-medium text-gray-900">
+                        Phone Number
+                    </label>
+                    <input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        placeholder="Phone number..."
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    />
+                    <div id="phone-error" arial-live="polite" aria-atomic="true">
+                        <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
+                    </div>
+                </div>
+                <div id="message-error" arial-live="polite" aria-atomic="true">
+                        <p className="mt-2 text-sm text-red-500">{state?.message}</p>
+                    </div>
+                <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 font medium rounded-sm text-sm w-full py-3 text-center">
+                    Save
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default CreateForm;
